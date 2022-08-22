8c378ea7ce3afa938e3660a19a8b8044d8bdd95b
З - 6 < - 6@DESKTOP-80ACB9M>
2022-08-22 21:08:19 +0500

    Добавьте файлы проекта.

---
+++ 22aug/Workers.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+class Florist {
+
+	std::string getName() {
+
+	}
+
+	void setName() {
+
+	}
+
+	int getSalary() {
+
+	}
+
+	void setSalary() {
+		
+	}
+
+private:
+	int salary;
+	std::string name;
+};
